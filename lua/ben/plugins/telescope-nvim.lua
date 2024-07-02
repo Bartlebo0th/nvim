a22226f23cacf44fe8f9cd122f1875b398a5f911
Ben Ingram <beningram1986@gmail.com>
2024-07-02 15:33:59 +0100

    add <leader>fk as a 'find keymaps' binding in telescope

--- lua/ben/plugins/telescope-nvim.lua
+++ lua/ben/plugins/telescope-nvim.lua
@@ -38,13 +38,17 @@ return {
 
 		telescope.load_extension("fzf")
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+		----- set keymaps -----
+
+		-- for conciseness
+		local keymap = vim.keymap
+		local builtin = require("telescope.builtin")
 
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find keymaps" })
 	end,
 }
